e52691147464126f7d2611a917f9f1821eebdd76
Mr Crypto <www.crypto@gmail.com>
2014-05-29 16:31:04 +0100

    Added the change to map sh from dash to bash and added wine installation

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-sudo apt-get update;
-sudo apt-get upgrade;
-sudo apt-get install vim flex bison m4 gawk gettext vim-gtk -y
+#sudo apt-get update;
+#sudo apt-get upgrade;
+
+#This is for changing the dash to bash
+#pushd /bin
+#sudo rm -rf sh
+#sudo ln -s bash sh
+#popd
+
+#Installing the tools for Development
+sudo apt-get install vim flex bison m4 gawk gettext vim-gtk wine -y
