773c1aa00d5aee0f893da08969b75f71487eaffb
Mr Crypto <www.crypto@gmail.com>
2014-05-29 16:51:55 +0100

    Added additional tools and support for Chrome installation

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -9,4 +9,27 @@
 #popd
 
 #Installing the tools for Development
-sudo apt-get install vim flex bison m4 gawk gettext vim-gtk wine -y
+#sudo apt-get install vim flex bison m4 gawk gettext vim-gtk wine build-essential -y
+
+#Installing another set of Tools for Development
+#sudo apt-get install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras  flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack lha arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg totem-mozilla icedax lame libmad0 libjpeg-progs filezilla ubuntu-restricted-extras libxss1 -y 
+
+#Installing the Kernel Headers
+#sudo apt-get install linux-headers-$(uname -r)
+
+#Installing Chrome
+if [[ $(getconf LONG_BIT) = "64" ]]
+then
+	echo "64bit Detected" &&
+	echo "Installing Google Chrome" &&
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+	sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+	rm -f google-chrome-stable_current_amd64.deb
+else
+	echo "32bit Detected" &&
+	echo "Installing Google Chrome" &&
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
+	sudo dpkg -i google-chrome-stable_current_i386.deb &&
+	rm -f google-chrome-stable_current_i386.deb
+fi
+
